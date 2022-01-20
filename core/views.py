d5cb2813cf50519d5fd90a4d1c86d66496c8de0f
DOUGLAS KELCZESKI <54138968+zigmon30@users.noreply.github.com>
2022-01-19 21:03:41 -0300

    criando autenticaçao
    criando autenticaçao para usuaarios do sistema logar e deslogar da aplicaçao

--- core/views.py
+++ core/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
+from django.contrib.auth import logout
 # Create your views here.
+@login_required(login_url='/login/')
+def index(request):
+    return render(request, 'index.html')
+
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+
+    logout(request)
+    return redirect('/login')
+
+@csrf_protect
+def submit_login(request):
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, 'Usuário/Senha inválidos. Favor tentar novamente.')
+    return redirect('/login/#')
--- sosadoteanimais/urls.py
+++ sosadoteanimais/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user),
+
+    path('',views.index)
+
 ]
