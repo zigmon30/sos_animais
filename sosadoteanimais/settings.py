c70456741a0252ad00f12359e00c829212794d80
DOUGLAS KELCZESKI <54138968+zigmon30@users.noreply.github.com>
2022-01-29 13:24:13 -0300

    up

--- sosadoteanimais/settings.py
+++ sosadoteanimais/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 import dj_database_url
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-q48$h(#zub)njefe37p2vx5a!j7)-gv$d9jl&axu=)p@bxp1b9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'sosadoteanimais.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'sosadoteanimais.herokuapp.com']
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL ='/media/'
